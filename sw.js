b452162fdc408ede85dab4e4d7c12bb7b2eac9c5
brightphachara11-ui <brightphachara11@gmail.com>
2026-09-23 16:42:08 +0700

    QR Beam v7: live version check (up to date / update available)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "qr-beam-v6";
-const FILES = ["./", "./index.html", "./manifest.json", "./icon-512.png"];
+const CACHE = "qr-beam-v7";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon-512.png", "./version.json"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
 // Network first: with internet you always get the newest version (and it is saved);
