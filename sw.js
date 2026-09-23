12bfd2332bf7700f99ac34eeb0893b0b303ff5c1
brightphachara11-ui <brightphachara11@gmail.com>
2026-09-23 12:33:23 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = "qr-beam-v2";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon-512.png"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request, { ignoreSearch: true }).then(r => r || fetch(e.request))); });
